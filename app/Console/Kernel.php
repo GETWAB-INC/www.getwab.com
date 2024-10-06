e775b2f7411d24728b0b035fdef34e3ad9c55608
ioborin22 <ioborin22@gmail.com>
2024-10-06 14:38:29 -0400

    20241006

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,9 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('send:helloemail')->everyMinute();
-
         $schedule->command('send:againemail')->everyMinute();
-
         $schedule->command('send:lastemail')->everyMinute();
     }
 
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
+// Existing command
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Your new scheduled commands
+Schedule::command('send:helloemail')->everyMinute();
+Schedule::command('send:againemail')->everyMinute();
+Schedule::command('send:lastemail')->everyMinute();
