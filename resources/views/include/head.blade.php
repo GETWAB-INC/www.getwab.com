34fcc0f99a7eb18a467fb59c538eaf7e99e4b2e4
ioborin22 <ioborin22@gmail.com>
2025-04-06 12:39:57 -0400

    20250406

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,8 +1,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/style-v.1.09.css') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/GetWab/Icone-3.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/GetWab/Icone-3.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/GetWab/Icone-3.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/GetWab/Icone.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/GetWab/Icone.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/GetWab/Icone.png') }}">
     <link rel="manifest" href="{{ asset('favicon_io/site.webmanifest') }}">
     <link rel="stylesheet" href="/images/GetWab/overused_grotesk/overused_grotesk.css">
