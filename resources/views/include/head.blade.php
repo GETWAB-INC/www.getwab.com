34cac465c6fe66f8c34adff5e77f526fa6802608
ioborin22 <ioborin22@gmail.com>
2025-04-06 12:29:11 -0400

    20250406

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,6 +1,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/style-v.1.07.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-v.1.08.css') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/GetWab/Icone-3.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/GetWab/Icone-3.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/GetWab/Icone-3.png') }}">
