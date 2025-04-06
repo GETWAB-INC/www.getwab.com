ad894ce0fdfc3c67e384eb204232eaeaaa694771
ioborin22 <ioborin22@gmail.com>
2025-04-06 12:26:57 -0400

    20250406

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,8 +1,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/style-v.1.07.css') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon_io/favicon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon_io/favicon.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon_io/favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/GetWab/Icone-3.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/GetWab/Icone-3.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/GetWab/Icone-3.png') }}">
     <link rel="manifest" href="{{ asset('favicon_io/site.webmanifest') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Gudea:wght@400;700&display=swap">
