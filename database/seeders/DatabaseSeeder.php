db6ff575d92d399dce8ce3d984376becf59cb474
ioborin22 <ioborin22@gmail.com>
2025-07-25 12:46:51 -0400

    20250725

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,18 +8,5 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        User::create([
-            'name' => 'Ilia',
-            'email' => 'ilia.oborin@getwabinc.com',
-            'password' => Hash::make('V%614ed5e692607d'),
-            'email_verified_at' => now(),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-    }
+    //
 }
