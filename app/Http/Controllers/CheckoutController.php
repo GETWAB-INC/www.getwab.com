5b6885c8a8697ca95d12d3930c90a3c1e138fe4f
ioborin22 <ioborin22@gmail.com>
2025-07-30 11:03:00 -0400

    20250730

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
@@ -15,10 +14,11 @@ class CheckoutController extends Controller
         $profile_id = '069E822B-906F-43F1-B7D1-57DE588E9AEF';
         $secret_key = 'a10e614ffbc24c49a26f761f331e856d796f7a33d1814829b67a144b2b0858972882a25998ae4d5886061fa692e64dffec8d356da72f4372915322e7c9ca63cd7d0c55b8455e40c19bd345bee8d515367d0206524eae4beea274ec96ce477f373dcf82515cbf417daf8697523e593b7763e2a38c6fff4b2ba210c3412e3774d9';
 
+        // 🔹 Основные поля
         $fields = [
             'access_key' => $access_key,
             'profile_id' => $profile_id,
-            'transaction_uuid' => uniqid(),
+            'transaction_uuid' => (string) Str::uuid(),
             'signed_date_time' => gmdate("Y-m-d\TH:i:s\Z"),
             'locale' => 'en',
             'transaction_type' => 'sale',
@@ -28,25 +28,29 @@ class CheckoutController extends Controller
             'payment_method' => 'card',
         ];
 
+        // 🔹 Указываем unsigned поля (вводит пользователь)
+        $fields['unsigned_field_names'] = 'card_number,card_expiry_date,card_cvn';
+
+        // 🔹 Обновляем signed_field_names (включаем unsigned_field_names!)
         $fields['signed_field_names'] = implode(',', array_keys($fields));
 
-        // Подпись
-        $message = implode(',', array_map(function ($k, $v) {
-            return "$k=$v";
-        }, array_keys($fields), array_values($fields)));
+        // 🔹 Генерация подписи
+        $message = implode(',', array_map(
+            fn($k, $v) => "$k=$v",
+            array_keys($fields), array_values($fields)
+        ));
 
         $fields['signature'] = base64_encode(hash_hmac('sha256', $message, $secret_key, true));
 
         return view('checkout.form', compact('fields'));
     }
 
-
     public function handleCallback(Request $request)
     {
         Log::info('🔁 Payment callback from FIS', $request->all());
 
         if ($request->input('decision') === 'ACCEPT') {
-            // TODO: обновить статус заказа в базе
+            // TODO: обновить статус заказа
         }
 
         return response('OK', 200);
@@ -56,8 +60,4 @@ class CheckoutController extends Controller
     {
         return view('checkout.result');
     }
-
-
-
-
 }
